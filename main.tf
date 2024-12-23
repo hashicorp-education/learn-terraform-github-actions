c45b1b4074940d07e0c17075780d8d946f0ce087
ChandlerTayek <chandlertayek@gmail.com>
2024-12-23 15:37:08 -0800

    Remove extra text

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-# TEST 12
+
 # resource "aws_security_group" "github-actions" {
 #   name        = "github-actions"
 #   description = "This is just a test for a github-actions tutorial."
